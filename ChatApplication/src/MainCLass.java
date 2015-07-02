ebcfd1546546c0b8cea2cf8956ca813ffa399b41
Archit Jain <gomzy08jain@gmail.com>
2015-07-02 01:58:46 -0700

    first commit

---
+++ ChatApplication/src/MainCLass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author archit08jain
+ */
+public class MainCLass {
+    
+    public static void main(String args[])
+    {
+        //MyServer server = new MyServer();
+        MyChatApp chatWindow = new MyChatApp();
+        chatWindow.setVisible(true);
+    }
+    
+}
